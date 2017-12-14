5e026233ad26056e7779f17b95a4a34c299f1bb4
Vedrulius <Otetc82@yandex.ru>
2017-12-14 22:25:38 +0300

    Modify tests

--- src/com/mihey/tests/SearcherTest.java
+++ src/com/mihey/tests/SearcherTest.java
@@ -1,41 +1,47 @@
 package com.mihey.tests;
 
 import org.junit.Assert;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SearcherTest {
     Searcher searcher;
 
-    @BeforeAll
-    public static void beforAll() {
-        System.out.println("Start test");
-    }
+//    @BeforeAll
+//    public static void beforAll() {
+//        System.out.println("Start test");
+//    }
 
-    @BeforeEach
+    @Before
     public void setUp() throws Exception {
         searcher = new Searcher();
     }
 
-    @Test
-    public void testIndexNotFound() throws Exception {
+    @org.junit.Test
+//    @Test
+    public void testIndexNotFound() {
         Assert.assertEquals(-1, searcher.search(new int[0], 0));
-        Assert.assertEquals(-1, searcher.search(new int[]{0, 1, 2, 3, 4, 5}, 7));
+        Assert.assertEquals(-8, searcher.search(new int[]{0, 1, 2, 3, 4, 5, 6}, 9));
+        Assert.assertEquals(-11, searcher.search(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 12));
     }
 
-    @AfterEach
-    public void cleanUp() {
-        System.out.println("Cleanup");
-    }
 
-    @AfterAll
-    public static void afterAll() {
-        System.out.println("Test finish");
+    @org.junit.Test
+    public void testIndexCorrect() {
+        Assert.assertEquals(0, searcher.search(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 0));
+        Assert.assertEquals(9, searcher.search(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 9));
+        Assert.assertEquals(4, searcher.search(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 4));
     }
 }
-
+//    @AfterEach
+//    public void cleanUp(){
+//        System.out.println("Cleanup");
+//    }
+//
+//    @AfterAll
+//    public static void afterAll() {
+//        System.out.println("Test finish");
+//    }
