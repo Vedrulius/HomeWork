35c5b3a6b64dc9e75c19cccb4b785283e2ca609d
Vedrulius <Otetc82@yandex.ru>
2017-12-30 17:35:44 +0300

    Added test

--- src/com/mihey/homework/collections/RandomListTest.java
+++ src/com/mihey/homework/collections/RandomListTest.java
@@ -4,19 +4,20 @@ import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RandomListTest {
     RandomList rl;
 
-    @Before
-    void setUp() {
+    @BeforeEach
+    public void setUp() {
         rl = new RandomList();
     }
 
     @Test
-    void generateRandomList() {
-
+    public void generateRandomList() {
+        assertEquals(20, rl.generateRandomList(20).size());
+        assertEquals(0, rl.generateRandomList(0).size());
     }
 
 }
