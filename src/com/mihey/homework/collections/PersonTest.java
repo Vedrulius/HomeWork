eeb3198de566fcf047400692796315458352106e
Vedrulius <Otetc82@yandex.ru>
2017-12-30 16:30:05 +0300

    Added class Person

---
+++ src/com/mihey/homework/collections/PersonTest.java
@@ -0,0 +1,12 @@
+package com.mihey.homework.collections;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    @Test
+    void compareTo() {
+    }
+
+}
