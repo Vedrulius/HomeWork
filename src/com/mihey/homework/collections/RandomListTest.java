9167709c6c2fe06e37acdfb739daeb575442d604
Vedrulius <Otetc82@yandex.ru>
2018-01-05 21:00:36 +0300

    Added test for empty string & null

--- src/com/mihey/homework/collections/RandomListTest.java
+++ src/com/mihey/homework/collections/RandomListTest.java
@@ -3,8 +3,9 @@ package com.mihey.homework.collections;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RandomListTest {
     RandomList rl;
@@ -18,6 +19,17 @@ class RandomListTest {
     public void generateRandomList() {
         assertEquals(20, rl.generateRandomList(20).size());
         assertEquals(0, rl.generateRandomList(0).size());
+        assertFalse(rl.generateRandomList(100).contains(null)&&rl.generateRandomList(100).contains(""));
     }
 
+    @Test
+    public void invalidRandomListLimit() {
+        assertThrows(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                rl.generateRandomList(-1);
+            }
+        });
+
+    }
 }
