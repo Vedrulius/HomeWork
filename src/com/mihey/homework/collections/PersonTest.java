ddc49c95c2d4e736bd52e931b17c5df5fe88428e
Vedrulius <Otetc82@yandex.ru>
2018-01-06 22:05:05 +0300

    Added Person test

--- src/com/mihey/homework/collections/PersonTest.java
+++ src/com/mihey/homework/collections/PersonTest.java
@@ -1,12 +1,25 @@
 package com.mihey.homework.collections;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
-    @Test
-    void compareTo() {
+
+    private Person person;
+
+    @BeforeEach
+    void setUp() {
+        person = new Person("Mikhail", "Dolgov");
     }
 
+    @Test
+    void compareTo() {
+        assertTrue(person.compareTo(new Person("Alex", "Dolgov")) > 0);
+        assertTrue(person.compareTo(new Person("Natalya", "Dolgov")) < 0);
+        assertTrue(person.compareTo(new Person("Mikhail", "Dolgov")) == 0);
+        assertTrue(person.compareTo(new Person("Mikhail", "Ivanov")) < 0);
+        assertTrue(person.compareTo(new Person("Mikhail", "Dolgoff")) > 0);
+    }
 }
