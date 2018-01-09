f1d5aa452fdeec313c2351722da95c743701d8d2
Vedrulius <Otetc82@yandex.ru>
2018-01-09 21:27:06 +0300

    Modify tests names

--- src/com/mihey/homework/collections/PersonTest.java
+++ src/com/mihey/homework/collections/PersonTest.java
@@ -15,10 +15,14 @@ class PersonTest {
     }
 
     @Test
-    void compareTo() {
+    void testBothNamesEquals() {
+        assertTrue(person.compareTo(new Person("Mikhail", "Dolgov")) == 0);
+    }@Test
+    void testNameIsDifferent() {
         assertTrue(person.compareTo(new Person("Alex", "Dolgov")) > 0);
         assertTrue(person.compareTo(new Person("Natalya", "Dolgov")) < 0);
-        assertTrue(person.compareTo(new Person("Mikhail", "Dolgov")) == 0);
+    }@Test
+    void testLastNameIsDifferent() {
         assertTrue(person.compareTo(new Person("Mikhail", "Ivanov")) < 0);
         assertTrue(person.compareTo(new Person("Mikhail", "Dolgoff")) > 0);
     }
