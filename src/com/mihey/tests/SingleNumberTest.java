43bbb301a0c761d4df672db3df5780454aab3c5b
Mikhail Dolgov <Otetc82@yandex.ru>
2020-05-01 17:58:12 +0300

    Adding test

---
+++ src/com/mihey/tests/SingleNumberTest.java
@@ -0,0 +1,26 @@
+package com.mihey.tests;
+
+import com.mihey.homework.leetcode.SingleNumber;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class SingleNumberTest {
+    SingleNumber snt;
+
+    @BeforeEach
+    void setUp() {
+        snt = new SingleNumber();
+    }
+
+    @Test
+    void testSingleNumber() {
+        assertEquals(9, snt.singleNumber(new int[]{1,1,2,2,5,5,9}));
+        assertEquals(5, snt.singleNumber(new int[]{1,1,2,2,5,9,9}));
+        assertEquals(0, snt.singleNumber(new int[]{}));
+        assertNotEquals(0, snt.singleNumber(new int[]{1,1,2,2,5,5,9}));
+    }
+}
+
