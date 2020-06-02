ba309220cff3bffd8e92f7a3fb8cdd99f0debd6e
Mikhail Dolgov <Otetc82@yandex.ru>
2020-06-02 21:13:32 +0300

    Adding oop package

---
+++ src/com/mihey/oop/Vehicle.java
@@ -0,0 +1,31 @@
+package com.mihey.oop;
+
+public class Vehicle extends  Object {
+    private int speed;
+    private int passengers;
+    void transporting (){}
+}
+
+class MotorVehicle extends Vehicle {
+
+}
+
+class Car extends MotorVehicle {
+
+}
+
+class Byke extends MotorVehicle {
+
+}
+
+class Aircraft extends Vehicle {
+
+}
+
+class Airplaine extends Aircraft {
+
+}
+
+class Helicopter extends Aircraft {
+
+}
