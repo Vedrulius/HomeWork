01fa552f0bd54c4df2fa91721452514e02e5c269
Mikhail Dolgov <otetc82@yandex.ru>
2020-09-04 09:17:51 +0300

    all

--- src/com/mihey/testpackage/Test.java
+++ src/com/mihey/testpackage/Test.java
@@ -1,24 +1,3 @@
 package com.mihey.testpackage;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.function.BiFunction;
-
-class Test {
-    public static <T> void applyFunction(T[] array, Function<T, T> func) {
-        // write your code here
-    }
-
-    // do not change code below
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String method = scanner.nextLine();
-        String[] array = scanner.nextLine().split(" ");
-        applyFunction(array,
-                "lower".equals(method) ? String::toLowerCase :
-                        "upper".equals(method) ? String::toUpperCase :
-                                "new".equals(method) ? String::new :
-                                        "trim".equals(method) ? String::trim : String::intern);
-        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
-    }
-}
+public class Test {}
